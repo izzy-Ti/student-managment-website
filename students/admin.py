f6ea27a816ef4baa0e801f919c861d1ef5a91d02
izzy-Ti <israelashenafi29@gmail.com>
2025-05-12 04:52:56 -0700

    student marklist added

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import student
-
-admin.site.register(student)
-
-
+'''
+from .models import Student
+from .models import Physics
+# Register your models here.
+'''
--- teachers/admin.py
+++ teachers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import mark
+from .models import student
 
-admin.site.register(mark)
+admin.site.register(student)
